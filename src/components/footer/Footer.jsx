fc6527b265c74a1e5db34431da7a77841ea555b0
Erez <erezmirav@gmail.com>
2023-02-13 15:26:46 +0200

    updated footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './footer.css';
-
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
 const Footer = () => {
   return (
     <footer>
@@ -24,6 +24,14 @@ const Footer = () => {
           <a href="#contact">Contact</a>
         </li>
       </ul>
+      <div className="footer__socials">
+        <a href="https://www.linkedin.com/in/erez-amirav/">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/ErezAmirav">
+          <FaGithub />
+        </a>
+      </div>
       <div className="footer__copyright">
         <small>Erez Amirav &copy; {new Date().getFullYear()}</small>
       </div>
